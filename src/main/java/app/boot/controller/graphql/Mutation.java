37e07cd1481e8d9d30a90890f3e5574d6ba7dc7d
ankurgoel92 <ankurgoel.juit@gmail.com>
2019-07-13 13:23:01 +0530

    GraphQL Server

---
+++ src/main/java/app/boot/controller/graphql/Mutation.java
@@ -0,0 +1,44 @@
+package app.boot.controller.graphql;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+
+import app.boot.exception.ApplicationNotFoundException;
+import app.boot.model.Application;
+import app.boot.repository.ApplicationRepository;
+
+@Component
+public class Mutation implements GraphQLMutationResolver {
+    private ApplicationRepository applicationRepository;
+
+    public Mutation(ApplicationRepository applicationRepository) {
+        this.applicationRepository = applicationRepository;
+    }
+
+    public Application newApplication(String name, String owner, String description) {
+        Application app = new Application(name, owner, description);
+        applicationRepository.save(app);
+        return app;
+    }
+
+    public boolean deleteApplication(Long id) {
+        applicationRepository.deleteById(id);
+        return true;
+    }
+
+    public Application updateApplicationOwner(String newOwner, Long id) {
+        Optional<Application> optionalApplication = applicationRepository.findById(id);
+
+        if (optionalApplication.isPresent()) {
+            Application application = optionalApplication.get();
+            application.setOwner(newOwner);
+            applicationRepository.save(application);
+            return application;
+        } else {
+            throw new ApplicationNotFoundException("Application Not Found", id);
+        }
+    }
+}
--- src/main/java/app/boot/exception/ApplicationNotFoundException.java
+++ src/main/java/app/boot/exception/ApplicationNotFoundException.java
@@ -1,9 +1,36 @@
 package app.boot.exception;
 
-public class ApplicationNotFoundException extends RuntimeException {
-    private static final long serialVersionUID = -336215399839698643L;
 
-    public ApplicationNotFoundException(String exception) {
-        super(exception);
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import graphql.ErrorType;
+import graphql.GraphQLError;
+import graphql.language.SourceLocation;
+
+public class ApplicationNotFoundException extends RuntimeException implements GraphQLError {
+
+    private static final long serialVersionUID = -4112549479329680904L;
+    private Map<String, Object> extensions = new HashMap<>();
+
+    public ApplicationNotFoundException(String message, Long invalidApplicationId) {
+        super(message);
+        extensions.put("invalidApplicationId", invalidApplicationId);
+    }
+
+    @Override
+    public List<SourceLocation> getLocations() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getExtensions() {
+        return extensions;
+    }
+
+    @Override
+    public ErrorType getErrorType() {
+        return ErrorType.DataFetchingException;
     }
 }
