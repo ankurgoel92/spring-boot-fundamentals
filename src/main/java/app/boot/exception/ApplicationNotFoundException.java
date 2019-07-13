ceaeb2f2f0efdf37a9230dcc6eff24e8db1d858c
ankurgoel92 <ankurgoel.juit@gmail.com>
2019-07-13 15:32:45 +0530

    removing graphql

--- src/main/java/app/boot/exception/ApplicationNotFoundException.java
+++ src/main/java/app/boot/exception/ApplicationNotFoundException.java
@@ -1,36 +1,12 @@
 package app.boot.exception;
 
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import graphql.ErrorType;
-import graphql.GraphQLError;
-import graphql.language.SourceLocation;
-
-public class ApplicationNotFoundException extends RuntimeException implements GraphQLError {
+public class ApplicationNotFoundException extends RuntimeException {
 
     private static final long serialVersionUID = -4112549479329680904L;
-    private Map<String, Object> extensions = new HashMap<>();
 
     public ApplicationNotFoundException(String message, Long invalidApplicationId) {
         super(message);
-        extensions.put("invalidApplicationId", invalidApplicationId);
+
     }
 
-    @Override
-    public List<SourceLocation> getLocations() {
-        return null;
-    }
-
-    @Override
-    public Map<String, Object> getExtensions() {
-        return extensions;
-    }
-
-    @Override
-    public ErrorType getErrorType() {
-        return ErrorType.DataFetchingException;
-    }
 }
